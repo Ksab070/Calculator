7e64265dc171ba4cfcb7a527a8cb74465606e501
Subhan Khan <78604028+Ksab070@users.noreply.github.com>
2022-12-22 20:31:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ const divide = function (x,y) {
     return x/y;
 }
 
+const operate = (operator,x,y) => {
+    switch (operator) {
+        case 1: add(x,y); break;
+        case 2: subtract(x,y); break;   
+        case 3: multiply(x,y); break;
+        case 4: divide(x,y); break;
+    }
+}
